5a7d0af593b3e95c0ac5b6ffb25d6872a4fa456e
DAVID MIRANDA <bydavid16@gmail.com>
2022-03-31 06:18:41 -0300

    ADD OPEN IN LEVELS CONSTANTES

--- src/utils/Contants.js
+++ src/utils/Contants.js
@@ -4,60 +4,71 @@ export const LEVELS = [
     level: 1,
     quantity: 3,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:true
+
   },
  {
     level: 2,
     quantity: 4,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 3,
     quantity: 5,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 4,
     quantity: 5,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 1,
     quantity: 6,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
  {
     level: 2,
     quantity: 6,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 3,
     quantity: 7,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 4,
     quantity: 7,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
    {
     level: 3,
     quantity: 8,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
   {
     level: 4,
     quantity: 8,
     star:[false,false,false],
-    points:0
+    points:0,
+    open:false
   },
 ];
